918681b43b3299477fb1e4362757c4df20ff5880
veesus mikel heir <memebigboy27@hotmail.com>
2019-07-11 13:20:05 -0500

    Added  ConfigSection

--- SRML/Config/ConfigElement.cs
+++ SRML/Config/ConfigElement.cs
@@ -2,33 +2,57 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
-
 namespace SRML.Config
 {
+
+    public class FieldBackedConfigElement : ConfigElement
+    {
+
+        protected FieldInfo field;
+        public override Type ElementType => field.FieldType;
+
+        protected override object Value
+        {
+            get { return field.GetValue(null); }
+            set { field.SetValue(null, value); }
+        }
+
+        public FieldBackedConfigElement(FieldInfo field) : base(GenerateDefaultOptions(field.FieldType,field.Name)) 
+        {
+            this.field = field;
+            Options.DefaultValue = Value;
+
+        }
+    }
     public class ConfigElement<T> : ConfigElement
     {
         public override Type ElementType => typeof(T);
 
-        public ConfigElement(ConfigElementOptions options)
+        protected override object Value { get; set; }
+
+
+
+        public ConfigElement(string name) : base(GenerateDefaultOptions(typeof(T),name))
         {
-            this.Options = options;
             if (Options.DefaultValue != null) SetValue(Options.DefaultValue);
-        }
-
-        public ConfigElement() : this(GenerateDefaultOptions(typeof(T)))
-        {
-
         }
     }
     public abstract class ConfigElement
     {
         public abstract Type ElementType { get; }
         public ConfigElementOptions Options { get; protected set; }
-        protected object Value;
+        protected abstract object Value { get; set; }
         public T GetValue<T>()
         {
             return (T)Value;
+        }
+
+        public ConfigElement(ConfigElementOptions options)
+        {
+            this.Options = options;
+            
         }
 
         public void SetValue<T>(T value)
@@ -36,32 +60,36 @@ namespace SRML.Config
             Value = value;
         }
 
-        public object GetValue()
-        {
-            return Value;
-        }
-
-        public void SetValue(object value)
-        {
-            Value = value;
-        }
-
-        public static ConfigElementOptions GenerateDefaultOptions(Type type)
+        public static ConfigElementOptions GenerateDefaultOptions(Type type,string name)
         {
             return new ConfigElementOptions()
             {
                 Parser = ParserRegistry.GetParser(type),
-                DefaultValue = type.IsValueType ? Activator.CreateInstance(type) : null
+                DefaultValue = type.IsValueType ? Activator.CreateInstance(type) : null,
+                Name = name
             };
         }
     }
-    
+
+    public class ConfigElementComparer : IEqualityComparer<ConfigElement>
+    {
+        public bool Equals(ConfigElement x, ConfigElement y)
+        {
+            return x.Options.Name == y.Options.Name;
+        }
+
+        public int GetHashCode(ConfigElement obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     public class ConfigElementOptions
     {
         public IStringParser Parser { get; internal set; }
-        public string Comment { get; internal set; } = "";
+        public string Comment { get; internal set; }
         public object DefaultValue { get; internal set; }
 
-
+        public string Name { get; internal set; }
     }
 }
---
+++ SRML/Config/ConfigSection.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using IniParser;
+using IniParser.Model;
+using UnityEngine;
+
+namespace SRML.Config
+{
+    public class ConfigSection
+    {
+        
+        readonly Dictionary<string, ConfigElement> elementDict = new Dictionary<string, ConfigElement>();
+        public void AddElement(ConfigElement element)
+        {
+            elementDict[element.Options.Name] = element;
+        }
+
+        public ConfigElement this[string index]
+        {
+            get
+            {
+                return elementDict[index];
+            }
+            set
+            {
+                elementDict[index] = value;
+            }
+        }
+        public void LoadIniData(KeyDataCollection keys)
+        {
+            foreach(var data in keys)
+            {
+                if(elementDict.TryGetValue(data.KeyName, out var element))
+                {
+                    element.SetValue(element.Options.Parser.ParseObject(data.Value));
+                }
+                else
+                {
+                    Debug.LogWarning($"Unknown key {data.KeyName} in section! Ignoring...");
+                }
+            }
+        }
+
+        public void WriteIniData(KeyDataCollection keys)
+        {
+            foreach(var element in elementDict)
+            {
+                var keyData = new KeyData(element.Key);
+                keyData.Value = element.Value.Options.Parser.EncodeObject(element.Value.GetValue<object>());
+                keyData.Comments = element.Value.Options.Comment != null ? new List<string> { element.Value.Options.Comment } : new List<string>();
+                if (!keys.AddKey(keyData)) Debug.LogWarning($"Key {element.Key} already present in section!");
+            }
+        }
+
+        static ConfigSection()
+        {
+            IniData file = new IniData();
+            var section = new SectionData("test");
+
+            var test = new ConfigSection();
+            test.AddElement(new ConfigElement<int>("testelement"));
+            var element = new ConfigElement<float>("testfloat");
+            element.Options.Comment = "Example Comment";
+            test.AddElement(element);
+            test.WriteIniData(section.Keys);
+            file.Sections.Add(section);
+            var parser = new FileIniDataParser();
+            parser.WriteFile(Path.Combine(Application.persistentDataPath, "test.ini"), file);
+
+        }
+    }
+}
