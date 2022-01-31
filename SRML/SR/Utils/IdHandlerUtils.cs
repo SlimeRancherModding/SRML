f9d53bb528eaad7c60fc13f4c9e784ddcb81b0ed
MegaPiggy <mehmehmeh313@gmail.com>
2022-01-31 16:55:43 -0500

    ed2

--- SRML/SR/Utils/IdHandlerUtils.cs
+++ SRML/SR/Utils/IdHandlerUtils.cs
@@ -29,7 +29,7 @@ namespace SRML.SR.Utils
         internal static Dictionary<Type, GameObject> idHandlerPrefabs = new Dictionary<Type, GameObject>();
         static IdHandlerUtils()
         {
-            SRCallbacks.PreSaveGameLoaded += (s) =>
+            SRCallbacks.PreSaveGameLoad += (s) =>
             {
                 foreach (IdHandler handler in UnityEngine.Object.FindObjectsOfType<IdHandler>())
                 {
