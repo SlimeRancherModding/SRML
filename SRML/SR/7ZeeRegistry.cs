d728af3a38a274993814350f195b70a6ae6ff664
Noah Pilarski <mehmehmeh313@gmail.com>
2022-05-06 20:17:36 -0400

    7Zee Rank Entries

---
+++ SRML/SR/7ZeeRegistry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SRML.SR
+{
+    public static class _7ZeeRegistry
+    {
+        internal static List<CorporatePartnerUI.RankEntry> moddedRankEntries = new List<CorporatePartnerUI.RankEntry>();
+
+        public static void RegisterRankEntry(CorporatePartnerUI.RankEntry rankEntry)
+        {
+            moddedRankEntries.Add(rankEntry);
+        }
+    }
+}
---
+++ SRML/SR/Patches/CorporatePartnerAwakePatch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HarmonyLib;
+
+namespace SRML.SR.Patches
+{
+    [HarmonyPatch(typeof(CorporatePartnerUI))]
+    [HarmonyPatch("Awake")]
+    internal static class CorporatePartnerAwakePatch
+    {
+        public static void Prefix(CorporatePartnerUI __instance)
+        {
+            List<CorporatePartnerUI.RankEntry> list = __instance.ranks.ToList();
+            foreach (CorporatePartnerUI.RankEntry rankEntry in _7ZeeRegistry.moddedRankEntries)
+            {
+                if (!list.Contains(rankEntry))
+                    list.Add(rankEntry);
+            }
+            __instance.ranks = list.ToArray();
+        }
+    }
+}
