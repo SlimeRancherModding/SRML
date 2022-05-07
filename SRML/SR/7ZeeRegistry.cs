12a81c6997813ac2547f4787f09813fa9744ec33
Noah Pilarski <mehmehmeh313@gmail.com>
2022-05-07 00:00:24 -0400

    Add level to entry

--- SRML/SR/7ZeeRegistry.cs
+++ SRML/SR/7ZeeRegistry.cs
@@ -1,16 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 
 namespace SRML.SR
 {
     public static class _7ZeeRegistry
     {
-        internal static List<CorporatePartnerUI.RankEntry> moddedRankEntries = new List<CorporatePartnerUI.RankEntry>();
+        internal static List<CorporatePartnerRankEntry> moddedRankEntries = new List<CorporatePartnerRankEntry>();
 
-        public static void RegisterRankEntry(CorporatePartnerUI.RankEntry rankEntry)
+        public static void RegisterRankEntry(CorporatePartnerRankEntry rankEntry)
         {
             moddedRankEntries.Add(rankEntry);
         }
+
+        public static void RegisterRankEntry(int level, CorporatePartnerUI.RankEntry rankEntry) => RegisterRankEntry(CorporatePartnerRankEntry.FromSR(level, rankEntry));
+    }
+
+    public class CorporatePartnerRankEntry
+    {
+        public int level;
+
+        public int cost;
+
+        public Sprite[] rewardIcons;
+
+        public Sprite rewardBanner;
+
+        public CorporatePartnerRankEntry(int level, int cost, Sprite rewardBanner, params Sprite[] rewardIcons)
+        {
+            this.level = level;
+            this.cost = cost;
+            this.rewardBanner = rewardBanner;
+            this.rewardIcons = rewardIcons;
+        }
+
+        public CorporatePartnerUI.RankEntry ToSR() => new CorporatePartnerUI.RankEntry
+        {
+            cost = this.cost,
+            rewardBanner = this.rewardBanner,
+            rewardIcons = this.rewardIcons
+        };
+
+        public static CorporatePartnerRankEntry FromSR(int level, CorporatePartnerUI.RankEntry rankEntry) => new CorporatePartnerRankEntry(level, rankEntry.cost, rankEntry.rewardBanner, rankEntry.rewardIcons);
     }
 }
--- SRML/SR/Patches/CorporatePartnerAwakePatch.cs
+++ SRML/SR/Patches/CorporatePartnerAwakePatch.cs
@@ -12,13 +12,42 @@ namespace SRML.SR.Patches
     {
         public static void Prefix(CorporatePartnerUI __instance)
         {
-            List<CorporatePartnerUI.RankEntry> list = __instance.ranks.ToList();
-            foreach (CorporatePartnerUI.RankEntry rankEntry in _7ZeeRegistry.moddedRankEntries)
+            int highestLevel = _7ZeeRegistry.moddedRankEntries.Max(rank => rank.level);
+            if (highestLevel < 28)
+                highestLevel = 28;
+
+            Dictionary<int, CorporatePartnerUI.RankEntry> dictionary = new Dictionary<int, CorporatePartnerUI.RankEntry>();
+            for (int i = 0; i < __instance.ranks.Length; i++)
+                dictionary.Add(i, __instance.ranks[i]);
+
+            foreach (CorporatePartnerRankEntry rankEntry in _7ZeeRegistry.moddedRankEntries)
             {
-                if (!list.Contains(rankEntry))
-                    list.Add(rankEntry);
+                if (!dictionary.ContainsKey(rankEntry.level))
+                    dictionary.Add(rankEntry.level, rankEntry.ToSR());
+                else
+                {
+                    CorporatePartnerUI.RankEntry oldRankEntry = dictionary[rankEntry.level];
+                    oldRankEntry.cost = rankEntry.cost;
+                    oldRankEntry.rewardBanner = rankEntry.rewardBanner;
+                    List<Sprite> joinedIcons = oldRankEntry.rewardIcons.ToList();
+                    joinedIcons.AddRange(rankEntry.rewardIcons);
+                    oldRankEntry.rewardIcons = joinedIcons.ToArray();
+                    dictionary[rankEntry.level] = oldRankEntry;
+                }
             }
-            __instance.ranks = list.ToArray();
+
+            for (int i = 0; i < highestLevel; i++)
+            {
+                if (!dictionary.ContainsKey(i))
+                    dictionary.Add(i, new CorporatePartnerUI.RankEntry
+                    {
+                        cost = 0,
+                        rewardBanner = null,
+                        rewardIcons = Array.Empty<Sprite>(),
+                    });
+            }
+
+            __instance.ranks = dictionary.OrderBy(kvp => kvp.Key).Select(kvp => kvp.Value).ToArray();
         }
     }
 }
