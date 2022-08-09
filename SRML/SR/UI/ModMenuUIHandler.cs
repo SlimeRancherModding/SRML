70dc80d644afe9ce906a7feb5c8ac33ad41cfd70
Lionmeow <49107380+Lionmeow@users.noreply.github.com>
2022-08-09 09:49:38 -0700

    version string, readd old dependency system

--- SRML/SR/UI/ModMenuUIHandler.cs
+++ SRML/SR/UI/ModMenuUIHandler.cs
@@ -13,6 +13,7 @@ namespace SRML.SR.UI
     internal static class ModMenuUIHandler
     {
         private static GameObject mainMenuUIPrefab;
+
         internal static void OnMainMenuLoaded(MainMenuUI ui)
         {
             MainMenuUtils.AddMainMenuButtonWithTranslation(ui, "ModsButton", "b.mods", () =>
@@ -22,13 +23,16 @@ namespace SRML.SR.UI
                 
             }).transform.SetSiblingIndex(5);
 
+            GameObject watermark = GameObject.Find("TitleUI/Watermark");
+            watermark.GetComponent<TMPro.TMP_Text>().alignment = TMPro.TextAlignmentOptions.Midline;
+            watermark.GetComponent<TMPro.TMP_Text>().text += $"\nSRML v {Main.VERSION_STRING}";
+            ((RectTransform)watermark.transform).anchoredPosition = new Vector2(0f, 12f);
+
             if (mainMenuUIPrefab) return;
 
             var h = Main.uiBundle.LoadAsset<GameObject>("ModPanel");
             h.AddComponent<ModMenuUI>().infoButtonPrefab = Main.uiBundle.LoadAsset<GameObject>("ModInfoButton");
             mainMenuUIPrefab = h;
         }
-
-
     }
 }
